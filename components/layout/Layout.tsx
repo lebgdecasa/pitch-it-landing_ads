d5fd5f2d37266d055311e013926d910759d9db67
Jad Lahrichi <jad.lahrichi@gmail.com>
2025-06-02 10:18:28 +0100

    analytics from vercel

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import Header from '@/components/layout/Header';
 import Footer from '@/components/layout/Footer';
 import { ReactNode } from 'react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/next';
+
 
 interface LayoutProps {
   children: ReactNode;
@@ -13,6 +16,8 @@ const Layout = ({ children, onOpenDemoModal }: LayoutProps) => {
     <>
       <Header onOpenDemoModal={onOpenDemoModal} />
       <main>{children}</main>
+      <SpeedInsights />
+      <Analytics />
       <Footer />
     </>
   );
